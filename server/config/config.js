ce4d40faede1e59bc5ba852f2553f56cc8cdf58b
winn92 <winn92091229@gmail.com>
2018-07-21 16:58:46 -0400

    Variable de entorno de MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -16,14 +16,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
   =============================
 */
 
-/*let urlDB;
+let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
-}*/
+}
 
-//process.env.URLDB = urlDB || process.env.MONGO_URI;
+process.env.URLDB = urlDB || process.env.MONGO_URI;
 
 console.log("Mongo URI", process.env.MONGO_URI);
-process.env.URLDB = process.env.MONGO_URI || 'mongodb://localhost:27017/cafe';
